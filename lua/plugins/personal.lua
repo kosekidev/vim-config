9b21df9899b9adb5f3a5c1ea2011d69c6fe416b7
KosekiDev <vincent.brehaut@gmail.com>
2023-03-14 20:13:31 +0100

    Add toggleterm to manage terminals in vim

--- lua/plugins/personal.lua
+++ lua/plugins/personal.lua
@@ -4,4 +4,13 @@ return {
   { "wellle/targets.vim" },
   { "windwp/nvim-ts-autotag" },
   { "tpope/vim-surround" },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = 12,
+      open_mapping = [[<c-\>]],
+      close_on_exit = true,
+    },
+  },
 }
