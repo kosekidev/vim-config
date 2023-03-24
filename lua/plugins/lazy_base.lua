511ba6e1cd30c480689aa0ccae5318d4eadcdc2d
KosekiDev <vincent.brehaut@gmail.com>
2023-03-24 19:27:45 +0100

    Useless indent decoration removed

--- lua/plugins/lazy_base.lua
+++ lua/plugins/lazy_base.lua
@@ -51,6 +51,8 @@ return {
   { "echasnovski/mini.bufremove", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
 
   -- change trouble config
   {
