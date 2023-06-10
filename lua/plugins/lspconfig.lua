791abbbfb5da1de8a31f0ee1f7917fa6cb74eba6
KosekiDev <vincent.brehaut@gmail.com>
2023-06-10 22:32:54 +0200

    update lsp keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -30,32 +30,57 @@ return {
 		lsp.on_attach(function(_, bufnr)
 			local opts = { buffer = bufnr, remap = false }
 
-			vim.keymap.set("n", "<leader>vws", function()
-				vim.lsp.buf.workspace_symbol()
-			end, opts)
 			vim.keymap.set("n", "<leader>vd", function()
 				vim.diagnostic.open_float()
 			end, opts)
 			vim.keymap.set("n", "<leader>ca", function()
 				vim.lsp.buf.code_action()
 			end, opts)
-			vim.keymap.set("n", "<leader>vr", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
+			vim.keymap.set("n", "<leader>cr", function()
 				vim.lsp.buf.rename()
 			end, opts)
-			vim.keymap.set("n", "[d", function()
+			vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+				vim.lsp.buf.format()
+			end, opts)
+
+			vim.keymap.set("n", "]d", function()
 				vim.diagnostic.goto_next()
 			end, opts)
-			vim.keymap.set("n", "]d", function()
+			vim.keymap.set("n", "[d", function()
 				vim.diagnostic.goto_prev()
 			end, opts)
+			vim.keymap.set("n", "]w", function()
+				vim.diagnostic.goto_next({
+					severity = "WARN",
+				})
+			end, opts)
+			vim.keymap.set("n", "[w", function()
+				vim.diagnostic.goto_prev({
+					severity = "WARN",
+				})
+			end, opts)
+			vim.keymap.set("n", "]e", function()
+				vim.diagnostic.goto_next({
+					severity = "ERROR",
+				})
+			end, opts)
+			vim.keymap.set("n", "[e", function()
+				vim.diagnostic.goto_prev({
+					severity = "ERROR",
+				})
+			end, opts)
+
 			vim.keymap.set("n", "gd", function()
 				vim.lsp.buf.definition()
 			end, opts)
+			vim.keymap.set("n", "gr", function()
+				vim.lsp.buf.references()
+			end, opts)
 			vim.keymap.set("n", "K", function()
 				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "gK", function()
+				vim.lsp.buf.signature_help()
 			end, opts)
 			vim.keymap.set("i", "<C-H>", function()
 				vim.lsp.buf.signature_help()
