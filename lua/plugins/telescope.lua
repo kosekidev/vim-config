18417cedc7efb7aaa93acfcc0809ca7d81dd7471
KosekiDev <vincent.brehaut@gmail.com>
2023-06-10 14:39:49 +0200

    remove some telescope remaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,14 +21,7 @@ return {
 		-- find
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 		{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-		-- git
-		{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-		{ "<leader>tgs", "<cmd>Telescope git_status<CR>", desc = "status" },
 		-- search
-		{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-		{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-		{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-		{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
 		{ "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
 		{ "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
 		{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
@@ -40,8 +33,6 @@ return {
 		{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
 		{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
 		{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-		{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-		{ "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
 		{
 			"<leader>ss",
 			function()
