f51cb075b60ffe735a502cb50c9ea7101943fcf0
KosekiDev <vincent.brehaut@gmail.com>
2023-06-12 07:25:51 +0200

    Change tabwith in option file

--- lua/kosekidev/options.lua
+++ lua/kosekidev/options.lua
@@ -4,9 +4,9 @@ vim.g.maplocalleader = " "
 vim.o.relativenumber = true
 vim.o.number = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.smartindent = true
 
