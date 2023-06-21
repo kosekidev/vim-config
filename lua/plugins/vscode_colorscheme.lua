4e1c5341a775d691fc8fa81bc99f8b53cfcb7b92
KosekiDev <vincent.brehaut@gmail.com>
2023-06-21 23:18:07 +0200

    Disabled vscode colorscheme

--- lua/plugins/vscode_colorscheme.lua
+++ lua/plugins/vscode_colorscheme.lua
@@ -1,10 +1,11 @@
 return {
-	"Mofiqul/vscode.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.api.nvim_set_hl(0, "@constant", { fg = "#4fc1ff", bg = "NONE" })
-		vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#4EC9B0", bg = "NONE" })
-		vim.cmd([[colorscheme vscode]])
-	end,
+  "Mofiqul/vscode.nvim",
+  enabled = false,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.api.nvim_set_hl(0, "@constant", { fg = "#4fc1ff", bg = "NONE" })
+    vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#4EC9B0", bg = "NONE" })
+    vim.cmd([[colorscheme vscode]])
+  end,
 }
