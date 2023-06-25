9359e1b4bcd3619290d09c810b42c4d1de7ada8d
KosekiDev <vincent.brehaut@gmail.com>
2023-06-25 14:38:40 +0200

    Add neotest custom extension on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,21 @@
+local neotest_extension = {
+  sections = {
+    lualine_a = {
+      function()
+        return 'NeoTest'
+      end
+    }
+  },
+  filetypes = {
+    'neotest-summary'
+  }
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = {
     theme = "catppuccin",
+    extensions = { neotest_extension }
   }
 }
