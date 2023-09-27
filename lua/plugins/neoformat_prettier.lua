34b86f487624f56c0f479e8dfa8afd17a6fe2758
KosekiDev <vincent.brehaut@gmail.com>
2023-09-27 20:33:45 +0200

    Remove guard and add neoformat for prettier formatting

---
+++ lua/plugins/neoformat_prettier.lua
@@ -0,0 +1,9 @@
+return {
+  "sbdchd/neoformat",
+  config = function()
+    vim.api.nvim_create_autocmd({"BufWritePre"}, {
+      pattern = {"*.js", "*.ts", "*.tsx", "*.css", "*.html", "*.vue", "*.json", "*.md"},
+      command = "Neoformat",
+    })
+  end
+}
