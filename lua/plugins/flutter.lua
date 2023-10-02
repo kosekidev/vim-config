93c2f87361f00384334d6a360794a21fedaaf625
KosekiDev <vincent.brehaut@gmail.com>
2023-10-02 20:07:17 +0200

    reformatting

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,9 +1,9 @@
 return {
-  'akinsho/flutter-tools.nvim',
-  lazy = false,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'stevearc/dressing.nvim',
-  },
-  config = true,
+	"akinsho/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim",
+	},
+	config = true,
 }
