9a52e1ba47e33e5873ca0d2a7e3db1ca089164e6
KosekiDev <vincent.brehaut@gmail.com>
2023-10-02 18:31:26 +0200

    Remove autoformat by the lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,73 +1,62 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require('lspconfig')
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
 
-    local on_attach = function(client, bufnr)
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("Format", { clear = true }),
-          buffer = bufnr,
-          callback = function() vim.lsp.buf.format() end
-        })
-      end
-    end
+		lspconfig.lua_ls.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+		lspconfig.tsserver.setup({})
+		lspconfig.jsonls.setup({})
+		lspconfig.html.setup({})
+		lspconfig.cssls.setup({})
 
-    lspconfig.lua_ls.setup {
-      on_attach = on_attach,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' }
-          },
-        }
-      }
-    }
-    lspconfig.tsserver.setup {}
-    lspconfig.jsonls.setup {}
-    lspconfig.html.setup {}
-    lspconfig.cssls.setup {}
+		vim.keymap.set("n", "<space>vd", vim.diagnostic.open_float, { desc = "Open float inline diagnostic" })
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diognostic" })
+		vim.keymap.set("n", "[w", function()
+			vim.diagnostic.goto_prev({
+				severity = "WARN",
+			})
+		end, { desc = "Go to next warning" })
+		vim.keymap.set("n", "]w", function()
+			vim.diagnostic.goto_next({
+				severity = "WARN",
+			})
+		end, { desc = "Go to next warning" })
+		vim.keymap.set("n", "[e", function()
+			vim.diagnostic.goto_prev({
+				severity = "ERROR",
+			})
+		end, { desc = "Go to previous error" })
+		vim.keymap.set("n", "]e", function()
+			vim.diagnostic.goto_next({
+				severity = "ERROR",
+			})
+		end, { desc = "Go to next error" })
 
-    vim.keymap.set('n', '<space>vd', vim.diagnostic.open_float, { desc = "Open float inline diagnostic" })
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diognostic" })
-    vim.keymap.set("n", "[w", function()
-      vim.diagnostic.goto_prev({
-        severity = "WARN",
-      })
-    end, { desc = "Go to next warning" })
-    vim.keymap.set("n", "]w", function()
-      vim.diagnostic.goto_next({
-        severity = "WARN",
-      })
-    end, { desc = "Go to next warning" })
-    vim.keymap.set("n", "[e", function()
-      vim.diagnostic.goto_prev({
-        severity = "ERROR",
-      })
-    end, { desc = "Go to previous error" })
-    vim.keymap.set("n", "]e", function()
-      vim.diagnostic.goto_next({
-        severity = "ERROR",
-      })
-    end, { desc = "Go to next error" })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local build_opts = function(keymap_desc)
+					return { buffer = ev.buf, remap = false, desc = keymap_desc }
+				end
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-      callback = function(ev)
-        local build_opts = function(keymap_desc)
-          return { buffer = ev.buf, remap = false, desc = keymap_desc }
-        end
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, build_opts("Go to declaration"))
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, build_opts("Go to definition"))
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, build_opts("Go to references"))
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, build_opts("Hover"))
-        vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, build_opts("Rename"))
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, build_opts("Code action"))
-      end
-    })
-  end
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, build_opts("Go to declaration"))
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, build_opts("Go to definition"))
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, build_opts("Go to references"))
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, build_opts("Hover"))
+				vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, build_opts("Rename"))
+				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, build_opts("Code action"))
+			end,
+		})
+	end,
 }
