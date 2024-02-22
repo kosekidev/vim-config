e210642ddabd9874e5dd7c37188678dae609bf9a
KosekiDev <vincent.brehaut@gmail.com>
2024-02-22 20:44:14 +0100

    Remove useless require

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -22,7 +22,7 @@ return {
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
