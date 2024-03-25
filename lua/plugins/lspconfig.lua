543b6f8c642f253674479a7dcbd72aeae6453aaa
KosekiDev <vincent.brehaut@gmail.com>
2024-03-26 00:00:41 +0100

    Adding support for volar

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,11 +12,40 @@ return {
 				},
 			},
 		})
-		lspconfig.tsserver.setup({})
 		lspconfig.jsonls.setup({})
 		lspconfig.html.setup({})
 		lspconfig.cssls.setup({})
 		lspconfig.rust_analyzer.setup({})
+
+		local current_path = vim.fn.expand("%:p:h")
+		local util = require("lspconfig.util")
+		local project_root = util.find_node_modules_ancestor(current_path)
+
+		local vue_path = util.path.join(project_root, "node_modules", "vue")
+		local is_vue = vim.fn.isdirectory(vue_path) == 1
+		if is_vue then
+			lspconfig.volar.setup({
+				filetypes = {
+					"vue",
+					"javascript",
+					"javascript.jsx",
+					"typescript",
+					"typescript.tsx",
+					"javascriptreact",
+					"typescriptreact",
+					"json",
+				},
+			})
+			lspconfig.tsserver.setup({
+				autostart = false,
+				root_dir = function()
+					return false
+				end,
+				single_file_support = false,
+			})
+		else
+			lspconfig.tsserver.setup({})
+		end
 
 		vim.keymap.set("n", "<space>vd", vim.diagnostic.open_float, { desc = "Open float inline diagnostic" })
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
