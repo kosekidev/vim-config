3b813e4681d2546b38312cee7d2bb39989b89fca
KosekiDev <vincent.brehaut@gmail.com>
2024-05-09 14:03:18 +0200

    Change smartindent for autoindent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
