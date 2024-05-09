f661575e89135dec6739c84c39055685085b39c8
KosekiDev <vincent.brehaut@gmail.com>
2024-05-09 14:03:38 +0200

    Add ensure_installed list for the configuration

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,7 +14,21 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"rust_analyzer",
+					"tsserver",
+					"js-debug-adapter",
+					"prettier",
+					"eslint",
+					"html",
+					"cssls",
+					"jsonls",
+					"volar",
+				},
+			})
 		end,
 	},
 }
