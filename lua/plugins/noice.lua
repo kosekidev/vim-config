f141f24baa4ce72ba7c5ca6000987e858e268e37
KosekiDev <vincent.brehaut@gmail.com>
2024-05-17 22:02:35 +0200

    remove noice keybindings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -69,29 +69,5 @@ return {
 			end,
 			desc = "Noice All",
 		},
-		{
-			"<c-f>",
-			function()
-				if not require("noice.lsp").scroll(4) then
-					return "<c-f>"
-				end
-			end,
-			silent = true,
-			expr = true,
-			desc = "Scroll forward",
-			mode = { "i", "n", "s" },
-		},
-		{
-			"<c-b>",
-			function()
-				if not require("noice.lsp").scroll(-4) then
-					return "<c-b>"
-				end
-			end,
-			silent = true,
-			expr = true,
-			desc = "Scroll backward",
-			mode = { "i", "n", "s" },
-		},
 	},
 }
