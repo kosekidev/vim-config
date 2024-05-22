bf59670542d7aa9c13d02e217f3c83e8a19c716f
KosekiDev <vincent.brehaut@gmail.com>
2024-05-22 17:42:36 +0200

    Fix nvim ts autotag setup

--- lua/plugins/ts_autotag.lua
+++ lua/plugins/ts_autotag.lua
@@ -1,6 +1,8 @@
 return {
 	"windwp/nvim-ts-autotag",
 	config = function()
-		require("nvim-ts-autotag").setup({ enable_close_on_slash = true })
+		require("nvim-ts-autotag").setup({
+			opts = { enable_close_on_slash = true },
+		})
 	end,
 }
