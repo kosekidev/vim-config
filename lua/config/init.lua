d9463b6d88680c15621f3e52f10239da27bbb2f2
KosekiDev <vincent.brehaut@gmail.com>
2024-07-06 14:09:09 +0200

    set light mode during the day

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,2 +1,8 @@
 require("config.options")
 require("config.remap")
+
+-- Show light theme automatically during the day
+local current_time = os.date("*t")
+if current_time.hour >= 9 and current_time.hour <= 20 then
+	vim.api.nvim_set_option("background", "light")
+end
