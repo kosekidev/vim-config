37c93e3d8e23ae9a0dec6c0826e7a4d44c275d55
KosekiDev <vincent.brehaut@gmail.com>
2024-07-08 19:08:47 +0200

    refacto

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -15,18 +15,22 @@ return {
 				on_attach = function(_, bufnr)
 					local crates = require("crates")
 
-					vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { silent = true, desc = "show crate's versions", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { silent = true, desc = "show crate's features", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, { silent = true, desc = "show crate's dependencies", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { silent = true, desc = "upgrade crate", buffer = bufnr })
-					vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { silent = true, desc = "upgrade crates", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, { silent = true, desc = "expand plain crate to inline table", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cX", crates.extract_crate_into_table, { silent = true, desc = "extract crate into table", buffer = bufnr })
-          vim.keymap.set("n", "<leader>cH", crates.open_homepage, { silent = true, desc = "open homepage", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cR", crates.open_repository, { silent = true, desc = "open repository", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cD", crates.open_documentation, { silent = true, desc = "open documentation", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cC", crates.open_crates_io, { silent = true, desc = "open crates.io", buffer = bufnr })
-					vim.keymap.set("n", "<leader>cL", crates.open_lib_rs, { silent = true, desc = "open lib.rs", buffer = bufnr })
+          local bind = function(mode, keymap, action, desc)
+            vim.keymap.set(mode, keymap, action, { silent = true, buffer = bufnr, desc = desc })
+          end
+
+					bind("n", "<leader>cv", crates.show_versions_popup,                "show crate's versions")
+					bind("n", "<leader>cf", crates.show_features_popup,                "show crate's features")
+					bind("n", "<leader>cd", crates.show_dependencies_popup,            "show crate's dependencies")
+					bind("n", "<leader>cU", crates.upgrade_crate,                      "upgrade crate")
+					bind("v", "<leader>cU", crates.upgrade_crates,                     "upgrade crates")
+					bind("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, "expand plain crate to inline table")
+					bind("n", "<leader>cX", crates.extract_crate_into_table,           "extract crate into table")
+          bind("n", "<leader>cH", crates.open_homepage,                      "open homepage")
+					bind("n", "<leader>cR", crates.open_repository,                    "open repository")
+					bind("n", "<leader>cD", crates.open_documentation,                 "open documentation")
+					bind("n", "<leader>cC", crates.open_crates_io,                     "open crates.io")
+					bind("n", "<leader>cL", crates.open_lib_rs,                        "open lib.rs")
 				end,
 				actions = true,
 				completion = true,
