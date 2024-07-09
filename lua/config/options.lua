ee1ce818aa98d9769cc456bfa512b27f6af14dfa
KosekiDev <vincent.brehaut@gmail.com>
2024-07-09 23:36:40 +0200

    feat(undotree): diff panel now disabled by default and open undotree autofocus the panel

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,3 +41,6 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
+
+vim.g.undotree_DiffAutoOpen = 0
+vim.g.undotree_SetFocusWhenToggle = 1
