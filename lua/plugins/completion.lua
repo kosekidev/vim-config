b442e34d7e767310d75320475a0f41c17b9e7cbc
KosekiDev <vincent.brehaut@gmail.com>
2024-09-07 11:52:49 +0200

    feat!: change <C-b> keymap for nvim-cmp to <C-d>
    <C-b> is used by my tmux config, so I need to tap 2 times <C-b> to
    scroll up.. And I don't like to tap 2 times the same keys for that.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -31,7 +31,7 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.close(),
