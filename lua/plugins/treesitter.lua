ad80c24d5ed619858b49d708ed324bd427242295
KosekiDev <vincent.brehaut@gmail.com>
2024-09-07 12:39:02 +0200

    feat: remove keymaps from treesitter that cause delay on visual mode
    Keymaps removed are for select left or right hand side of an assignment,
    that I don't really care.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -45,8 +45,6 @@ return {
 					keymaps = {
 						["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
 						["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-						["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-						["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 					},
 				},
 				move = {
