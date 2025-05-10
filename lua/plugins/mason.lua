32936f3a291d36d4a54e6ad60a9e29587bd25731
KosekiDev <vincent.brehaut@gmail.com>
2025-05-10 17:35:44 +0200

    fix: disable automatic_enable for mason-lspconfig

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -15,6 +15,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
+				automatic_enable = false,
 				ensure_installed = {
 					"lua_ls",
 					"rust_analyzer",
