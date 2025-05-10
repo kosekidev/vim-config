30b35edba2b899cfc860618620e84779699a4c95
KosekiDev <vincent.brehaut@gmail.com>
2025-05-10 17:59:44 +0200

    fix: mini.ai breaking change

--- lua/plugins/mini_nvim.lua
+++ lua/plugins/mini_nvim.lua
@@ -11,9 +11,9 @@ return {
 				o = ai.gen_spec.treesitter({
 					a = { "@conditional.outer", "@loop.outer" },
 					i = { "@conditional.inner", "@loop.inner" },
-				}, {}),
-				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+				}),
+				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
 			},
 		})
 	end,
