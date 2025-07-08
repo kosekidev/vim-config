add6f0b0934c121859318114d163b221c4df9252
KosekiDev <vincent.brehaut@gmail.com>
2025-07-08 18:30:21 +0200

    chore: rename mason repo

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		lazy = false, -- Load immediately to ensure PATH is set
 		cmd = "Mason",
 		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
